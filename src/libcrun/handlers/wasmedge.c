7489761067c24909ca4d75e760f1ef9b3fd5d4f9
ysd-rysn <81076133+ysd-rysn@users.noreply.github.com>
2022-10-23 11:14:54 +0000

    wasm: add new custom handler handler_wasmedge_with_multiple_wasm

--- src/libcrun/handlers/wasmedge.c
+++ src/libcrun/handlers/wasmedge.c
@@ -153,9 +153,106 @@ libwasmedge_exec (void *cookie, __attribute__ ((unused)) libcrun_container_t *co
 }
 
 static int
+libwasmedge_exec_multiple_wasm (void *cookie, __attribute__ ((unused)) libcrun_container_t *container, const char *pathname, char *const argv[])
+{
+  WasmEdge_ConfigureContext *(*WasmEdge_ConfigureCreate) (void);
+  void (*WasmEdge_ConfigureDelete) (WasmEdge_ConfigureContext * Cxt);
+  void (*WasmEdge_ConfigureAddProposal) (WasmEdge_ConfigureContext * Cxt, const enum WasmEdge_Proposal Prop);
+  void (*WasmEdge_ConfigureAddHostRegistration) (WasmEdge_ConfigureContext * Cxt, enum WasmEdge_HostRegistration Host);
+  WasmEdge_VMContext *(*WasmEdge_VMCreate) (const WasmEdge_ConfigureContext *ConfCxt, WasmEdge_StoreContext *StoreCxt);
+  void (*WasmEdge_VMDelete) (WasmEdge_VMContext * Cxt);
+  WasmEdge_Result (*WasmEdge_VMRegisterModuleFromFile) (WasmEdge_VMContext * Cxt, WasmEdge_String ModuleName, const char *Path);
+  WasmEdge_Result (*WasmEdge_VMRunWasmFromFile) (WasmEdge_VMContext * Cxt, const char *Path, const WasmEdge_String FuncName, const WasmEdge_Value *Params, const uint32_t ParamLen, WasmEdge_Value *Returns, const uint32_t ReturnLen);
+  bool (*WasmEdge_ResultOK) (const WasmEdge_Result Res);
+  WasmEdge_String (*WasmEdge_StringCreateByCString) (const char *Str);
+  uint32_t n_wasm = 0; // number of wasm files
+  const char *dirs[1] = { "/:/" };
+  WasmEdge_ConfigureContext *configure;
+  WasmEdge_VMContext *vm;
+  WasmEdge_Result result;
+
+  WasmEdge_ModuleInstanceContext *wasi_module;
+  WasmEdge_ModuleInstanceContext *(*WasmEdge_VMGetImportModuleContext) (WasmEdge_VMContext * Cxt, const enum WasmEdge_HostRegistration Reg);
+  void (*WasmEdge_ModuleInstanceInitWASI) (WasmEdge_ModuleInstanceContext * Cxt, const char *const *Args, const uint32_t ArgLen, const char *const *Envs, const uint32_t EnvLen, const char *const *Dirs, const uint32_t DirLen, const char *const *Preopens, const uint32_t PreopenLen);
+  WasmEdge_ModuleInstanceInitWASI = dlsym (cookie, "WasmEdge_ModuleInstanceInitWASI");
+
+  WasmEdge_ConfigureCreate = dlsym (cookie, "WasmEdge_ConfigureCreate");
+  WasmEdge_ConfigureDelete = dlsym (cookie, "WasmEdge_ConfigureDelete");
+  WasmEdge_ConfigureAddProposal = dlsym (cookie, "WasmEdge_ConfigureAddProposal");
+  WasmEdge_ConfigureAddHostRegistration = dlsym (cookie, "WasmEdge_ConfigureAddHostRegistration");
+  WasmEdge_VMCreate = dlsym (cookie, "WasmEdge_VMCreate");
+  WasmEdge_VMDelete = dlsym (cookie, "WasmEdge_VMDelete");
+  WasmEdge_VMRegisterModuleFromFile = dlsym (cookie, "WasmEdge_VMRegisterModuleFromFile");
+  WasmEdge_VMGetImportModuleContext = dlsym (cookie, "WasmEdge_VMGetImportModuleContext");
+  WasmEdge_VMRunWasmFromFile = dlsym (cookie, "WasmEdge_VMRunWasmFromFile");
+  WasmEdge_ResultOK = dlsym (cookie, "WasmEdge_ResultOK");
+  WasmEdge_StringCreateByCString = dlsym (cookie, "WasmEdge_StringCreateByCString");
+
+  if (WasmEdge_ConfigureCreate == NULL || WasmEdge_ConfigureDelete == NULL || WasmEdge_ConfigureAddProposal == NULL
+      || WasmEdge_ConfigureAddHostRegistration == NULL || WasmEdge_VMCreate == NULL || WasmEdge_VMDelete == NULL
+      || WasmEdge_VMRegisterModuleFromFile == NULL || WasmEdge_VMGetImportModuleContext == NULL
+      || WasmEdge_ModuleInstanceInitWASI == NULL || WasmEdge_VMRunWasmFromFile == NULL
+      || WasmEdge_ResultOK == NULL || WasmEdge_StringCreateByCString == NULL)
+    error (EXIT_FAILURE, 0, "could not find symbol in `libwasmedge.so.0`");
+
+  configure = WasmEdge_ConfigureCreate ();
+  if (UNLIKELY (configure == NULL))
+    error (EXIT_FAILURE, 0, "could not create wasmedge configure");
+
+  WasmEdge_ConfigureAddProposal (configure, WasmEdge_Proposal_BulkMemoryOperations);
+  WasmEdge_ConfigureAddProposal (configure, WasmEdge_Proposal_ReferenceTypes);
+  WasmEdge_ConfigureAddProposal (configure, WasmEdge_Proposal_SIMD);
+  WasmEdge_ConfigureAddHostRegistration (configure, WasmEdge_HostRegistration_Wasi);
+
+  vm = WasmEdge_VMCreate (configure, NULL);
+  if (UNLIKELY (vm == NULL))
+    {
+      WasmEdge_ConfigureDelete (configure);
+      error (EXIT_FAILURE, 0, "could not create wasmedge vm");
+    }
+
+  wasi_module = WasmEdge_VMGetImportModuleContext (vm, WasmEdge_HostRegistration_Wasi);
+  if (UNLIKELY (wasi_module == NULL))
+    {
+      WasmEdge_VMDelete (vm);
+      WasmEdge_ConfigureDelete (configure);
+      error (EXIT_FAILURE, 0, "could not get wasmedge wasi module context");
+    }
+
+  for (char *const *arg = argv; *arg != NULL; ++arg, ++argn)
+    ;
+
+  WasmEdge_ModuleInstanceInitWASI (wasi_module, (const char *const *) &argv[0], argn, NULL, 0, dirs, 1, NULL, 0);
+
+  result = WasmEdge_VMRunWasmFromFile (vm, pathname, WasmEdge_StringCreateByCString ("_start"), NULL, 0, NULL, 0);
+
+  if (UNLIKELY (! WasmEdge_ResultOK (result)))
+    {
+      WasmEdge_VMDelete (vm);
+      WasmEdge_ConfigureDelete (configure);
+      error (EXIT_FAILURE, 0, "could not get wasmedge result from VM");
+    }
+
+  WasmEdge_VMDelete (vm);
+  WasmEdge_ConfigureDelete (configure);
+  exit (EXIT_SUCCESS);
+}
+
+static int
 wasmedge_can_handle_container (libcrun_container_t *container, libcrun_error_t *err arg_unused)
 {
   return wasm_can_handle_container (container, err);
+}
+
+static int
+wasmedge_can_handle_multiple_wasm (libcrun_container_t *container, libcrun_error_t *err arg_unused)
+{
+  if (wasm_can_handle_container (container, err))
+	{
+	  return can_handle_multiple_wasm (container, err);
+	}
+
+  return 0;
 }
 
 struct custom_handler_s handler_wasmedge = {
@@ -165,6 +262,15 @@ struct custom_handler_s handler_wasmedge = {
   .unload = libwasmedge_unload,
   .exec_func = libwasmedge_exec,
   .can_handle_container = wasmedge_can_handle_container,
+};
+
+struct custom_handler_s handler_wasmedge_with_multiple_wasm = {
+  .name = "wasmedge_with_multiple_wasm",
+  .feature_string = "WASM:wasmedge_with_multiple_wasm",
+  .load = libwasmedge_load,
+  .unload = libwasmedge_unload,
+  .exec_func = libwasmedge_exec_multiple_wasm,
+  .can_handle_container = wasmedge_can_handle_multiple_wasm,
 };
 
 #endif
